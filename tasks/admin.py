869ab9ffe0a7e68c918430be9759ee0476f3205a
Luiz Felipe Neves <luizfneves@proton.me>
2026-04-21 12:41:11 -0300

    tasks no admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
-from .models import TaskList
+
+from .models import Task, TaskList
 
 
 @admin.register(TaskList)
 class TaskListAdmin(admin.ModelAdmin):
     list_display = ("name", "user", "created_at")
     search_fields = ("name", "user__username")
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "task_list",
+        "owner",
+        "status",
+        "priority",
+        "due_date",
+        "planned_date",
+    )
+    search_fields = ("title", "task_list__name", "owner__username")
